83efde5eb7feb56ae5731204e80ec156e524d1a7
Estb <estevaoaugusto@hotmail.com>
2020-08-12 00:23:13 -0300

    Update stockController.js
    Add check stock

--- src/controllers/stockController.js
+++ src/controllers/stockController.js
@@ -107,15 +107,9 @@ exports.oneProduct = async (req, res, next) => {
             menssage: "Product not found.",
           });
 
-        this.verifyForecast()
-          .POST(product.amount, product.forecastSale, product.sku, userId)
-          .then((vs) => {
-            product.status = this.verifyForecast().GET(
-              product.dataValues.status
-            );
-            httpResponseHelper.ok(res, product);
-          })
-          .catch((error) => next(error));
+        product.status = this.verifyForecast().GET(product.status);
+
+        httpResponseHelper.ok(res, product);
       })
       .catch((error) => next(error));
   } catch (err) {
@@ -134,7 +128,10 @@ exports.allProduct = async (req, res, next) => {
             status: 404,
             menssage: "Product not found.",
           });
-
+        products.map(element =>{
+        element.status = this.verifyForecast().GET(element.status);
+        })
+        
         httpResponseHelper.ok(res, products);
       })
       .catch((error) => next(error));
@@ -176,14 +173,53 @@ exports.deleteProduct = async (req, res, next) => {
 };
 
 exports.verifyForecast = () => {
+  let am = 0;
+  let fs = 0;
+
+  __check = async (sku, userId) => {
+    let t;
+    const p1 = await new Promise(async (resolve, reject) => {
+      await stdaos()
+        .findAll(userId)
+        .then((stores) => {
+          let i = 0;
+          stores.forEach(async (element) => {
+            await daos()
+              .findOne(sku, element.storeId)
+              .then((prod) => {
+                if (prod) {
+                  if (prod.status == "1") {
+                    am += prod.amount;
+                    fs += prod.forecastSale;
+                  }
+                }
+              })
+              .finally(() => {
+                i++;
+                if (i == stores.length) {
+                  resolve();
+                }
+              });
+          });
+        });
+    });
+    const p2 = await new Promise((resolve) => {
+      resolve((t = am - fs > 0 ? "2" : "1"));
+    });
+    const promises = [p1, p2];
+    await Promise.all(promises)
+    return t;
+  };
   return {
     POST: async (amount, forecastSale, sku, userId) => {
       if (amount < forecastSale) {
-      return status = 2;
+        var ns = await __check(sku, userId);
+        return (status = 2 + ns);
       } else if (amount - forecastSale >= 20) {
         status = 1;
       } else if (amount - forecastSale >= 0 || amount - forecastSale < 20) {
-          return status = 3;
+        var ns = await __check(sku, userId);
+        return (status = 3 + ns);
       }
       return status;
     },
@@ -191,12 +227,18 @@ exports.verifyForecast = () => {
       if (status == "1") {
         status =
           "Quantity of this product in the store is sufficient for the current sales forecast";
-      } else if (status == "2") {
+      } else if (status == "21") {
         status =
-          "Warning:: This month's sales forecast is higher than the current stock for this product in this store.";
-      } else if (status == "3") {
+          "Warning:: This month's sales forecast is higher than the current stock for this product in this store. You don't have enough stock available at another store.";
+      } else if (status == "22") {
         status =
-          "Attention: Considering the current sales forecast for this store, the quantity of the current product, although sufficient, is very close to the limit.";
+          "Warning:: This month's sales forecast is higher than the current stock for this product in this store. You have this product available with stock in another store";
+      } else if (status == "31") {
+        status =
+          "Attention: Considering the current sales forecast for this store, the quantity of the current product, although sufficient, is very close to the limit. You don't have enough stock available at another store.";
+      } else if (status == "32") {
+        status =
+          "Attention: Considering the current sales forecast for this store, the quantity of the current product, although sufficient, is very close to the limit. You have this product available with stock in another store.";
       }
       return status;
     },
